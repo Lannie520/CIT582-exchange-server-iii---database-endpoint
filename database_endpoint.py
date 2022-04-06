79b3cc329acb540de17390b231dc6d6ed14937fe
lannie520 <33674399+Lannie520@users.noreply.github.com>
2022-04-07 01:23:41 +0800

    edit

--- database_endpoint.py
+++ database_endpoint.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask, request, g
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine, select, MetaData, Table
@@ -8,8 +9,8 @@ import algosdk
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import load_only
+
 from models import Base, Order, Log
-from datetime import datetime
 
 engine = create_engine('sqlite:///orders.db')
 Base.metadata.bind = engine
@@ -38,12 +39,11 @@ def shutdown_session(response_or_exc):
 
 
 def log_message(d):
-    # Takes input dictionary d and writes it to the Log table
     g.session.add(Log(logtime=datetime.now(), message=json.dumps(d)))
     g.session.commit()
 
 
-def appendOrder(order, data):
+def attachData(order, data):
     data.append({
         'sender_pk': order.sender_pk,
         'receiver_pk': order.receiver_pk,
@@ -87,7 +87,6 @@ def trade():
             return jsonify(False)
 
         # Your code here
-        # Each order should be a dict with (at least) the following fields ("sender_pk", "receiver_pk", "buy_currency", "sell_currency", "buy_amount", "sell_amount", “signature”).
         signature = content['sig']
         payload = json.dumps(content['payload'])
         sender_public_key = content['payload']['sender_pk']
@@ -98,15 +97,10 @@ def trade():
         sell_amount = content['payload']['sell_amount']
         platform = content['payload']['platform']
 
-        # The platform must be either “Algorand” or "Ethereum". Your code should check whether “sig” is a valid signature of json.dumps(payload), using the signature algorithm specified by the platform field. Be sure to sign the payload using the sender_pk.
-        # If the signature verifies, all of the fields under the ‘payload’ key should be stored in the “Order” table EXCEPT for 'platform’.
-        # If the signature does not verify, do not insert the order into the “Order” table.
         if platform == 'Algorand':
             if algosdk.util.verify_bytes(payload.encode('utf-8'), signature, sender_public_key):
-                # print("Algo sig verifies!")
                 # Note that you can access the database session using g.session
-                g.session.add(Order(sender_pk=sender_public_key, receiver_pk=receiver_public_key,
-                              buy_currency=buy_currency, sell_currency=sell_currency, buy_amount=buy_amount, sell_amount=sell_amount, signature=signature))
+                g.session.add(Order(sender_pk=sender_public_key, receiver_pk=receiver_public_key, buy_currency=buy_currency, sell_currency=sell_currency, buy_amount=buy_amount, sell_amount=sell_amount, signature=signature))
                 g.session.commit()
                 return jsonify(True)
             else:
@@ -114,33 +108,26 @@ def trade():
                 return jsonify(False)
 
         elif platform == 'Ethereum':
-            eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
+            eth_message = eth_account.messages.encode_defunct(text=payload)
 
-            if eth_account.Account.recover_message(eth_encoded_msg, signature=signature) == sender_public_key:
-                # print("Eth sig verifies")
+            if eth_account.Account.recover_message(eth_message, signature=signature) == sender_public_key:
                 # Note that you can access the database session using g.session
-                g.session.add(Order(sender_pk=sender_public_key, receiver_pk=receiver_public_key,
-                              buy_currency=buy_currency, sell_currency=sell_currency, buy_amount=buy_amount, sell_amount=sell_amount, signature=signature))
+                g.session.add(Order(sender_pk=sender_public_key, receiver_pk=receiver_public_key, buy_currency=buy_currency, sell_currency=sell_currency, buy_amount=buy_amount, sell_amount=sell_amount, signature=signature))
                 g.session.commit()
                 return jsonify(True)
             else:
                 log_message(content)
                 return jsonify(False)
 
-        # Instead, insert a record into the “Log” table, with the message field set to be json.dumps(payload).
-        # In this assignment, you will not need to match or fill the orders, simply insert them into the database (if the signature verifies).
-
-
+      
 @app.route('/order_book')
 def order_book():
     # Your code here
     # Note that you can access the database session using g.session
-    # The “/order_book” endpoint should return a list of all orders in the database. 
-    # The response should contain a single key “data” that refers to a list of orders formatted as JSON.
+   
     data = []
     for order in g.session.query(Order).all():
-        appendOrder(order, data)
-        # print(order)
+        attachData(order, data)
     return jsonify(data=data)
 
 
